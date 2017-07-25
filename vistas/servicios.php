c54f8819f73a3ce654dd99b691ce5442da77b3b3
esteban1234 <tva_libra@hotmail.com>
2017-07-25 18:07:09 -0500

    servicios listo

--- vistas/servicios.php
+++ vistas/servicios.php
@@ -1,6 +1,178 @@
 <!DOCTYPE html>
 <?php include 'encabezado2.php' ?>
 <body>
+	<header>
+  <nav class="pull-left">
+    <ul>
+      <a class="enlaces" href="../index.php">INICIO</a>
+      <a class="enlaces" href="nosotros.php">NOSOTROS</a>
+      <a class="enlaces" id="select" href="servicios.php">SERVICIOS</a>
+      <a class="enlaces" href="contacto.php">CONTACTO</a>
+    </ul>
+  </nav>
+  <figure class=" logo pull-right">
+    <img src="../img/padma.png" class="img-responsive" alt="">
+  </figure>
+</header>
+
+<div class="banner3">
+    <div class="fondo3">
+    	<p style="color: #fff;">SERVICIOS</p>
+    	<div style="text-align: center; margin-top: 5%;">
+      <a style="color: #fff;" class="btn btn-danger btn-outline btn-lg" href="contacto.php">CONTACTANOS</a>
+    </div>
+    </div>
+</div>
+
+
+<div class="container servicios">
+	<p class="texto">Con años de experiencia representando marcas líderes a nivel mundial, contamos con un servicio Técnico de calidad. Con amplia experiencia en los sectores, portuario, logístico, minero y de construcción. Somos personas organizadas para una atención personalizada, brindando Servicio Técnico los 365 días del año.
+	</p>
+	<div class="row">
+	    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 card">
+    		<a href="#">
+    		<div class="thumbnail">
+    		    <div style="background-color:  #566573; height: 100px;" ></div>
+    			<div class="caption">
+    				<h3>
+    					Mantenimiento Preventivo
+    					<!-- <small>
+    						Lorem ipsum dolor sit amet
+    					</small> -->
+    				</h3>
+    				<hr>		
+    				<p>
+    					Inspección periódica programada y planeada por los ingenieros encargados sobre el trabajo en todas las áreas de la planta con el fin de detectar el uso y desgaste de equipos y maquinas u otros y detectar posibles fallas o errores que puedan ocurrir en los lugares de trabajo.
+    				</p>
+    				<!-- <button class="btn btn-default" role="button">View More</button> -->
+    			</div>
+    		</div>
+    		</a>
+    	</div>
+    	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 card">
+    		<a href="#">
+    		<div class="thumbnail">
+    		    <div style="background-color:  #566573; height: 100px;" ></div>
+    			<div class="caption">
+    				<h3>
+    					Mantenimiento Full
+    					<!-- <small>
+    						Lorem ipsum dolor sit amet
+    					</small> -->
+    				</h3>
+    				<hr>		
+    				<p>
+    					Servicio de mantenimiento para su equipo ligero, pesado ó especializado manejando todas las marcas disponibles, entregando un analisis del estado de su equipo.
+    				</p>
+    				<!-- <button class="btn btn-default" role="button">View More</button> -->
+    			</div>
+    		</div>
+    		</a>
+    	</div>
+    	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 card">
+    		<a href="#">
+    		<div class="thumbnail">
+    		    <div style="background-color:  #566573; height: 100px;" ></div>
+    			<div class="caption">
+    				<h3>
+    					Repuestos
+    					<!-- <small>
+    						Lorem ipsum dolor sit amet
+    					</small> -->
+    				</h3>
+    				<hr>		
+    				<p>
+    					Las marcas de los productos que distribuimos son fabricadas siguiendo rigurosos procedimientos de control de calidad, lo que nos permite ofrecer partes con estandares iguales a los del equipo original.
+    				</p>
+    				<!-- <button class="btn btn-default" role="button">View More</button> -->
+    			</div>
+    		</div>
+    		</a>
+    	</div>
+    	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 card">
+    		<a href="#">
+    		<div class="thumbnail">
+    		    <div style="background-color:  #566573; height: 100px;" ></div>
+    			<div class="caption">
+    				<h3>
+    					Servicio Técnico
+    					<!-- <small>
+    						Lorem ipsum dolor sit amet
+    					</small> -->
+    				</h3>
+    				<hr>		
+    				<p>
+    					Nuestro grupo humano está capacitado y certificado en las fábricas de producción y provisto de los mejores equipos y herramientas, para solucionar de manera inmediata cualquier inconveniente que presente una máquina.
+    				</p>
+    				<!-- <button class="btn btn-default" role="button">View More</button> -->
+    			</div>
+    		</div>
+    		</a>
+    	</div>
+	</div>
+</div>
+
+<article class="petroleo">
+	<div class="section">
+      <div class="container">
+       <h1 style="margin-top: 7%; margin-bottom: 7%;" class="text-center">Supervisión de construcción de obras para petróleo y gas</h1>
+        <div class="row">
+          <div class="col-md-3">
+            <div style="text-align: center;">
+            	<i class="fa fa-fire fa-5x" aria-hidden="true"></i>
+            </div> <br><br>
+            <h4 class="text-center">Construcción de gasoductos y ramales de alimentación.</h4>
+          </div>
+          <div class="col-md-3">
+            <div style="text-align: center;">
+            	<i class="fa fa-fire fa-5x" aria-hidden="true"></i>
+            </div> <br><br>
+            <h4 class="text-center">Construcción de oleoductos y cañerías para inyección.</h4>
+          </div>
+          <div class="col-md-3">
+            <div style="text-align: center;">
+            	<i class="fa fa-fire fa-5x" aria-hidden="true"></i>
+            </div> <br><br>
+            <h4 class="text-center">Plantas reguladoras de presión, puentes de medición y trampas de scrapers.</h4>
+          </div>
+          <div class="col-md-3">
+            <div style="text-align: center;">
+            	<i class="fa fa-fire fa-5x" aria-hidden="true"></i>
+            </div> <br><br>
+            <h4 class="text-center">Instalaciones industriales de gas.</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+ <br><br><br>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div style="text-align: center;">
+            	<i class="fa fa-fire fa-5x" aria-hidden="true"></i>
+            </div> <br><br>
+            <h4 class="text-center">Redes de incendio.</h4>
+          </div>
+          <div class="col-md-4">
+            <div style="text-align: center;">
+            	<i class="fa fa-fire fa-5x" aria-hidden="true"></i>
+            </div> <br><br>
+            <h4 class="text-center">Remediamiento de suelos contaminados.</h4>
+          </div>
+          <div class="col-md-4">
+            <div style="text-align: center;">
+            	<i class="fa fa-fire fa-5x" aria-hidden="true"></i>
+            </div> <br><br>
+            <h4 class="text-center">Reparación de ductos, parcheos y clock spring.</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div style="text-align: center; margin-top: 5%; margin-bottom: 5%;">
+      <a style="color: #fff;"  class="btn btn-danger btn-outline btn-lg" href="contacto.php">CONTACTANOS</a>
+    </div>
+</article>
 	
 <?php include 'pie.php' ?>
 <script src="../js/jquery.js"></script>
