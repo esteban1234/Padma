87605c5622bc5e3d91678cd88c21f688c67b7959
esteban1234 <tva_libra@hotmail.com>
2017-07-25 16:55:57 -0500

    nosotros listo

--- vistas/nosotros.php
+++ vistas/nosotros.php
@@ -1,7 +1,122 @@
 <!DOCTYPE html>
 <?php include 'encabezado2.php' ?>
 <body>
-	
+	<header>
+  <nav class="pull-left">
+    <ul>
+      <a class="enlaces" href="../index.php">INICIO</a>
+      <a class="enlaces" id="select" href="nosotros.php">NOSOTROS</a>
+      <a class="enlaces" href="servicios.php">SERVICIOS</a>
+      <a class="enlaces" href="contacto.php">CONTACTO</a>
+    </ul>
+  </nav>
+  <figure class=" logo pull-right">
+    <img src="../img/padma.png" class="img-responsive" alt="">
+  </figure>
+</header>
+
+<div class="banner2">
+    <p>LA EMPRESA</p>
+</div>
+
+<article class="somos">
+	<h1>SERVICIOS Y MANTENIMIENTO PADMA S.C.P.</h1>
+	<p class="texto"><strong>Padma</strong> es una empresa especializada en la  asistencia técnica de maquinaria de industria, excavación y camiones de igual manera ocupa servicios en Administración y supervisión de construcción de obras para petróleo y gas.  Nuestro éxito se basa en conocer las necesidades de nuestros clientes y en proporcionarles el mejor servicio. <br><br> 
+
+	Nos implicamos en los proyectos de nuestros clientes para garantizarles un servicio de calidad. En estos años, hemos conseguido su confianza gracias a nuestros puntos fuertes. Asesoramiento y Servicio preventivo y correctivo.
+</p>	
+</article>
+
+<div class="mvv"> <br><br><br>
+<div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h2 class="text-center">MISION</h2> <br>
+            <p>Brindar servicios que cubran las necesidades de nuestros clientes, proporcionando el mejor resultado al mejor precio, utilizando todos los recursos para entregar todas las solicitudes en las mejores condiciones de funcionamiento.</p>
+          </div>
+          <div class="col-md-6">
+            <h2 class="text-center">VISION</h2> <br>
+            <p>Ser los líderes en el mercado de Reparación y mantenimiento de maquinaria y equipo industrial y administración y supervisión de obras para petróleo y gas. Manteniendo siempre los mejores estándares establecidos en nuestros principios, valores y misión.</p>
+          </div>
+        </div>
+      </div>
+    </div> <br><br><br>
+      <div class="container">
+        <div class="row">
+          <div class=" col-md-4 col-md-offset-4">
+            <h2 class="text-center">Valores</h2> <br>
+            <p>
+            	<p class="pull-left">
+            	    * Trabajo en equipo <br>
+            		* Responsabilidad <br>
+            		* Honestidad <br>
+            		* Respeto <br>
+            		* Calidad 
+            	</p>
+            	
+            	<p class="pull-right">
+            	    * Rentabilidad <br>
+            	    * Puntualidad <br>
+            	    * Seguridad <br>
+            		* Lealtad <br>
+            		* Servicio
+            		        	
+            	</p>
+            </p>
+          </div>
+          
+        </div>
+      </div>
+	<br><br><br>
+</div>
+
+<div class="section">
+      <div class="container" style="margin-top: 5%; margin-bottom: 5%;">
+        <div class="row">
+          <div class="col-md-12">
+            <h1 class="text-center">¿Por que Elegir Padma?</h1>
+            <!-- <p class="text-center">We are a group of skilled individuals.</p> -->
+          </div>
+        </div> <br><br><br><br>
+        <div class="row">
+          <div class="col-md-2 text-right">
+            <i class="fa fa-usd fa-5x" aria-hidden="true"></i>
+          </div>
+          <div class="col-md-4">
+            <h3 class="text-left">Excelentes Precios</h3>
+            <p class="text-left">Precios competitivos en todos los servicios. ¡No hay cargos ocultos de ningún tipo!</p>
+          </div>
+          <div class="col-md-2 text-right">
+            <i class="fa fa-pencil-square-o fa-5x" aria-hidden="true"></i>
+          </div>
+          <div class="col-md-4">
+            <h3 class="text-left">Cotize Fácil Online</h3>
+            <p class="text-left"> Rellene nuestro formulario de contacto, y le enviaremos la cotización que usted necesita.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-2 text-right">
+            <i class="fa fa-clock-o fa-5x" aria-hidden="true"></i>
+          </div>
+          <div class="col-md-4">
+            <h3 class="text-left">Períodos flexibles</h3>
+            <p class="text-left">Servicio disponible los 365 dias del año.</p>
+          </div>
+          <div class="col-md-2 text-right">
+            <i class="fa fa-phone fa-5x" aria-hidden="true"></i>
+          </div>
+          <div class="col-md-4 text-center">
+            <h3 class="text-left">Asistencia telefónica</h3>
+            <p class="text-left">Estamos disponibles de lunes a viernes de 9 am a 6 pm.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+<div style="text-align: center; margin-top: 5%; margin-bottom: 5%;">
+      <a   class="btn btn-danger btn-outline btn-lg" href="vistas/servicios.php">CONTACTANOS</a>
+    </div>
+
 
 <?php include 'pie.php' ?>
 <script src="../js/jquery.js"></script>
