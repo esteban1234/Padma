401c5bfbd61b244c4c456befeae0a4c698778676
esteban1234 <tva_libra@hotmail.com>
2017-07-25 09:41:51 -0500

    carpetas

---
+++ index.php
@@ -0,0 +1,102 @@
+
+<?php include 'vistas/encabezado.php' ?>
+<body>
+
+<div class="contenlogocontacto">
+  <figure class="logo pull-left">
+    <a href="index.php"><img src="img/condesif2.png" alt="" class="img-responsive"></a>
+  </figure>
+  <a href="vistas/contacto.php" class="boton"><button type="button" class="btn btn-danger pull-right">Contactar</button></a>
+</div>
+<div id="header__icon" class="menu">
+  <i id="open" class="fa fa-align-justify fa-2x" aria-hidden="true"></i>
+  <i id="close" class="fa fa-close fa-2x close"  aria-hidden="true"></i>
+  <p>Menu</p>
+</div>
+<nav>
+<ul>
+  <li class="select"><a href="index.php">INDEX</a></li>
+  <li><a href="vistas/nosotros.php">NOSOTROS</a></li>
+  <li><a href="vistas/servicios.php">SERVICIOS</a></li>
+  <li><a href="vistas/contacto.php">CONTACTO</a></li>
+</ul>
+</nav>
+
+<div class="banner">
+  <!-- <div class="fondo"> -->
+    <div class="fondo2">
+      <p>
+        <h1>Desarrollando proyectos de alto estandar tecnológico por todos lados</h1>
+        <p class="textob">En el desarrollo de todos nuestros proyectos
+         tenemos siempre en cuenta el cumplimiento
+         de las directrices de calidad y medioambientales.</p>
+      </p>
+    </div>
+  <!-- </div> -->
+</div>
+
+<div class="content bg-info">
+  <div class="section uno">
+      <div class="container">
+        <div class="row ">
+          <div class="col-xs-12 col-sm-12 col-md-6 unotexto">
+            <h2 class="text-primary">Inspírate para crear el espacio que siempre soñaste</h2>
+            <p class="lead">Utiliza los servicios que CONSTRUCTORA CONSEDIF te ofrece para tomar las mejores decisiones de acabados y protección. ¡contacta a CONSTRUCTORA CONSEDIF para crear el espacio de tus sueños! </p>
+            <a href="vistas/contacto.php" class="boton"><button type="button" class="btn btn-danger pull-right">Contactar</button></a>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-6">
+            <img src="img/inte.jpg" class="img-responsive">
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+
+<div class="seccion">
+  <div class="izqui">
+    <div class="izquiuno">
+      <h1 style="color: #fff;">Algunos servicios</h1>
+      <p style="color: #fff;">Ejecutamos cualquier tipo de proyecto, servicio o mantenimiento en las ramas de la ingeniería civil y afines en las mejores condiciones de tiempo, costo y calidad respaldada por maquinaria y equipos propios.</p>
+      <a href="vistas/contacto.php" class="boton2"><button type="button" class="btn btn-danger pull-right">Contactar</button></a>
+    </div>
+    <div class="izquidos">
+      <img src="img/contructora.jpg" alt="" class="img-responsive">
+    </div>
+  </div>
+  <div class="dere">
+          <div class="col-xs-12 col-sm-6 col-md-6">
+            <img src="img/arqui.jpg" class="img-responsive">
+            <h2 style="font-size: 20px;">Proyectos Arquitectónicos</h2>
+            <p>Contamos con arquitectos especializados en planos, para que su desarrollo sea de total calidad antes durante y despues de cada proyecto.</p>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-6">
+            <img src="img/edi.jpg"  class="img-responsive">
+            <h2 style="font-size: 20px;">Edificación</h2>
+            <p>Contamos con la asesoría técnica y profesional para la realización de proyecto y Construcción de edificios de cualquier giro que sea su empresa.</p>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-6">
+            <img src="img/estudios2.png"     class="img-responsive">
+            <h2 style="font-size: 20px;">Estudios preliminares</h2>
+            <p>Analizamos, interpretamos, utilizamos y coordinamos los estudios preliminares de obra, proporcionando los datos e información que te permitirán conocer las características y condiciones del terreno a construir.</p>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-6">
+            <img src="img/diseno.jpg" class="img-responsive">
+            <h2 style="font-size: 20px;">Diseño</h2>
+            <p class="centrar">Brindamos servicios de arquitectura y especialidades para todo tipo de proyectos.</p>
+          </div>
+  </div>
+</div>
+<div class="bannerinfo">
+  <p>Construimos con las conservacion del medio ambiente</p>
+  <h2>¡SOLO TENEMOS UN MUNDO CUIDALO!</h2>
+</div>
+
+<?php include 'vistas/pie.php' ?>
+<script src="js/jquery-2.2.3.min.js"></script>
+<!-- <script src="js/bootstrap.js"></script> -->
+<script src="js/menu.js"></script>
+</body>
+</html>
